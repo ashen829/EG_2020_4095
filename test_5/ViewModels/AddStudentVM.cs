8eefe5e9b2b06c1157ec1d358a9cd1e1a97d2259
ashen829 <ashennethsara99@gmail.com>
2023-07-15 22:01:41 +0530

    Add project files.

---
+++ test_5/ViewModels/AddStudentVM.cs
@@ -0,0 +1,135 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using test_5.Models;
+using test_5.Views;
+
+namespace test_5.ViewModels
+{
+    public partial class AddStudentVM : ObservableObject
+    {
+        public void Back()
+        {
+            var vm = new MainWindowVM();
+            MainWindow window = new MainWindow();
+            window.ShowDialog();
+
+            
+        }
+
+
+
+        [ObservableProperty]
+        public string firstname;
+
+
+        [ObservableProperty]
+        public string lastname;
+
+        [ObservableProperty]
+        public int age;
+
+        [ObservableProperty]
+        public string dateofbirth;
+
+        [ObservableProperty]
+        public double gpa;
+
+        [ObservableProperty]
+        public BitmapImage selectedImage;
+
+        public Student student { get; private set; }
+        public Action CloseAction { get; internal set; }
+        public AddStudentVM(Student x)
+        {
+            student = x;
+
+            firstname = student.FirstName;
+            lastname = student.LastName;
+            age = student.Age;
+            gpa = student.GPA;
+            dateofbirth = student.DateOfBirth;
+            selectedImage = student.Image;
+
+        }
+        public AddStudentVM()
+        {
+
+        }
+
+        [RelayCommand]
+        public void InsertImage()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.bmp; *.png; *.jpg";
+            dialog.FilterIndex = 1;
+            if (dialog.ShowDialog() == true)
+            {
+                selectedImage = new BitmapImage(new Uri(dialog.FileName));
+
+                MessageBox.Show("Image uploded!", "successfull");
+            }
+        }
+
+        [RelayCommand]
+        public void Save()
+        {
+
+
+
+            if (gpa < 0 || gpa > 4)
+            {
+                MessageBox.Show("Please enter a GPA value between 0 and 4 ");
+                return;
+            }
+            if (student == null)
+            {
+
+                student = new Student()
+                {
+                    FirstName = firstname,
+                    LastName = lastname,
+                    Age = age,
+                    DateOfBirth = dateofbirth,
+                    Image = selectedImage,
+                    GPA = gpa
+
+                };
+
+
+            }
+            else
+            {
+
+                student.FirstName = firstname;
+                student.LastName = lastname;
+                student.Age = age;
+                student.GPA = gpa;
+                student.Image = selectedImage;
+                student.DateOfBirth = dateofbirth;
+
+
+
+            }
+
+            if (student.FirstName != null)
+            {
+
+                CloseAction();
+            }
+            Application.Current.MainWindow.Show();
+
+
+        }
+
+
+
+    }
+}
---
+++ test_5/ViewModels/MainWindowVM.cs
@@ -0,0 +1,114 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using test_5.Models;
+using test_5.Views;
+
+namespace test_5.ViewModels
+{
+    public partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<Student> students;
+        [ObservableProperty]
+        public Student selectedStudent = null;
+
+
+
+        public MainWindowVM()
+        {
+            students = new ObservableCollection<Student>();
+
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/U1.png", UriKind.Relative));
+            students.Add(new Student(23, "Ashen", "Nethsara", "29/08/1999", image1));
+
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/U3.png", UriKind.Relative));
+            students.Add(new Student(18, "Malindu", "Methsara", "12/1/2006", image2));
+
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/U4.png", UriKind.Relative));
+            students.Add(new Student(23, "Tharindu", "Perera", "02/11/1999", image3));
+
+            BitmapImage image4 = new BitmapImage(new Uri("/Model/Images/U5.png", UriKind.Relative));
+            students.Add(new Student(22, "Ranasinghe", "Kavindu", "15/07/2000", image4));
+
+        }
+
+        [RelayCommand]
+        public void CloseWindow()
+        {
+            Application.Current.MainWindow.Close();
+
+        }
+
+        [RelayCommand]
+        public void MinimizeWindow()
+        {
+           
+
+        }
+
+        [RelayCommand]
+        public void AddNewStudent()
+        {
+            var vm = new AddStudentVM();
+            AddStudent window = new AddStudent(vm);
+            window.ShowDialog();
+
+            if (vm.student.FirstName != null)
+            {
+                students.Add(vm.student);
+            }
+        }
+
+        [RelayCommand]
+        public void DeleteStudent()
+        {
+            if (selectedStudent != null)
+            {
+                string name = selectedStudent.FirstName;
+                students.Remove(selectedStudent);
+                MessageBox.Show("Deleted successfull!", "DELETED \a ");
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Student before Delete.", "Error");
+
+
+            }
+        }
+
+        [RelayCommand]
+        public void EditStudent()
+        {
+            if (selectedStudent != null)
+            {
+                var vm = new AddStudentVM(selectedStudent);
+                
+                var window = new AddStudent(vm);
+
+                window.ShowDialog();
+
+
+                int index = students.IndexOf(selectedStudent);
+                students.RemoveAt(index);
+                students.Insert(index, vm.student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student", "Warning!");
+            }
+        }
+
+    }
+}
